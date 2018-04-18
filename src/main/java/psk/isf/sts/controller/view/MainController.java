ff536f5f0335e862cc0364596cc7b78adf3a0ec1
ArtAmb <artur.ambrolewicz@gmail.com>
2018-04-19 01:09:02 +0200

    first commit

---
+++ src/main/java/psk/isf/sts/controller/view/MainController.java
@@ -0,0 +1,18 @@
+package psk.isf.sts.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	public static String templateDirRoot = "";
+
+	private String getTemplateDir(String templateName) {
+		return templateDirRoot + templateName;
+	}
+
+	@GetMapping("/")
+	public String getMainView() {
+		return getTemplateDir("mainMenu");
+	}
+}
