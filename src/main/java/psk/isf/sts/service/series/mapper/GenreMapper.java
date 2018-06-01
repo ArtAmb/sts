592c7258b9be2c85b762e53f06fd512fc5cb883c
Karolina <karolinabatkowska@op.pl>
2018-06-01 21:20:23 +0200

    Gatunki mapper

---
+++ src/main/java/psk/isf/sts/service/series/mapper/GenreMapper.java
@@ -0,0 +1,14 @@
+package psk.isf.sts.service.series.mapper;
+
+import psk.isf.sts.entity.Genre;
+import psk.isf.sts.service.series.dto.GenreDTO;
+
+public class GenreMapper {
+
+	public static GenreDTO map(Genre g) {
+		return GenreDTO.builder()
+				.id(g.getId())
+				.name(g.getName())
+				.build();
+	}
+}
